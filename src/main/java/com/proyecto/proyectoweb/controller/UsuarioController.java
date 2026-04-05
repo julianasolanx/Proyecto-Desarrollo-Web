cbbb47503796731eb7f890d0354cab640fa10ac3
SaraRU68 <149622071+Nataly1000@users.noreply.github.com>
2026-04-04 22:26:46 -0500

    Se añade login

--- src/main/java/com/proyecto/proyectoweb/controller/UsuarioController.java
+++ src/main/java/com/proyecto/proyectoweb/controller/UsuarioController.java
@@ -1,6 +1,10 @@
 package com.proyecto.proyectoweb.controller;
+import com.proyecto.proyectoweb.dto.EmailRequestDTO;
 import com.proyecto.proyectoweb.dto.UsuarioDTO;
+import com.proyecto.proyectoweb.service.EmailService;
 import com.proyecto.proyectoweb.service.UsuarioService;
+
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UsuarioController {
     private final UsuarioService usuarioService;
+    private final EmailService emailService;
 
     @GetMapping
     public ResponseEntity<List<UsuarioDTO>> listar() {
@@ -33,6 +38,17 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.crearUsuario(dto));
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UsuarioDTO dto) {
+    boolean existe = usuarioService.login(dto.getCorreo(), dto.getContrasena());
+    if (existe) {
+        return ResponseEntity.ok("OK");
+    } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Usuario no encontrado");
+    }
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<UsuarioDTO> actualizar(@PathVariable Long id, @RequestBody UsuarioDTO dto) {
         return ResponseEntity.ok(usuarioService.actualizarUsuario(id, dto));
@@ -43,4 +59,11 @@ public class UsuarioController {
         usuarioService.eliminarUsuario(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping("/enviar-invitacion")
+    public ResponseEntity<String> enviarInvitacion(@RequestBody EmailRequestDTO dto) {
+        emailService.enviarCorreo(dto.getTo(), dto.getSubject(), dto.getMessage());
+        return ResponseEntity.ok("Correo enviado correctamente");
+    }
+
 }
