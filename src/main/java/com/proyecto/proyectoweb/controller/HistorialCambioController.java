5493c86d1191495e244cd9c9699891bbed20ad68
joseval29 <josecastiblanco2910@gmail.com>
2026-05-06 07:13:37 -0500

    feat: croosorigin

--- src/main/java/com/proyecto/proyectoweb/controller/HistorialCambioController.java
+++ src/main/java/com/proyecto/proyectoweb/controller/HistorialCambioController.java
@@ -4,6 +4,7 @@ import com.proyecto.proyectoweb.dto.HistorialCambioDTO;
 import com.proyecto.proyectoweb.service.HistorialCambioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/proyecto/proyectoweb/controller/RolProcesoController.java
+++ src/main/java/com/proyecto/proyectoweb/controller/RolProcesoController.java
@@ -4,6 +4,7 @@ import com.proyecto.proyectoweb.service.RolProcesoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
