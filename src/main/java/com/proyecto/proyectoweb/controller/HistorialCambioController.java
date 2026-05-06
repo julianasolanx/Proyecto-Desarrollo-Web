4a69761e787d79ee14b8f51dcd4c69452ceaf292
Juan Bello <141571725+Juan-Bello@users.noreply.github.com>
2026-05-05 19:12:14 -0500

    Add CrossOrigin annotation to HistorialCambioController

--- src/main/java/com/proyecto/proyectoweb/controller/HistorialCambioController.java
+++ src/main/java/com/proyecto/proyectoweb/controller/HistorialCambioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/historial")
 @RequiredArgsConstructor
 public class HistorialCambioController {
