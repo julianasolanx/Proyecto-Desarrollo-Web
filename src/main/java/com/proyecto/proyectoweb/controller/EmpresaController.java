d0b2eee797e577cba52f3f4e2a98d1c1f30af781
Juan Bello <141571725+Juan-Bello@users.noreply.github.com>
2026-05-05 19:12:55 -0500

    Add CrossOrigin annotation to EmpresaController

--- src/main/java/com/proyecto/proyectoweb/controller/EmpresaController.java
+++ src/main/java/com/proyecto/proyectoweb/controller/EmpresaController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/empresas")
 @RequiredArgsConstructor
 public class EmpresaController {
