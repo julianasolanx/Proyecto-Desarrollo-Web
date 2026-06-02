243cc1697282dc173eeb1ba1b5020cd50f281d82
Juliana Novoa <julianansolano@gmail.com>
2026-06-01 21:40:07 -0500

    ResourceServerApplication.java en controller

---
+++ src/main/java/com/proyecto/proyectoweb/controller/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyectoweb.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerApplication.class, args);
+	}
+
+}
